bfb0f0ec755166b24fd5b256b977eba602d25692
Luke Turner <github@luketurner.org>
2017-11-11 21:00:58 -0800

    Add minimum viable drag-and-drop

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -3,7 +3,13 @@ import * as React from 'react';
 
 import Layout from './layout';
 import { GlobalStore } from '../store';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
-export default observer<{ store: GlobalStore }>(({ store }) => 
+export interface UIProps {
+  store: GlobalStore
+}
+
+export default DragDropContext(HTML5Backend)(observer<UIProps>(({ store }) => 
   <Layout store={store} />
-);
+));
--- src/ui/node-view/index.tsx
+++ src/ui/node-view/index.tsx
@@ -2,6 +2,7 @@ import { action } from 'mobx';
 import { observer } from 'mobx-react';
 import * as React from 'react';
 import * as CSSModule from 'react-css-modules';
+import { DragSource, DropTarget } from 'react-dnd';
 
 import { SNode, NodeType } from '../../nodes';
 import nodeStore from '../../nodes/store';
@@ -15,23 +16,64 @@ export interface NodeViewProps {
   nodeId: string
 }
 
-export default CSSModule(observer<NodeViewProps>(({ nodeId }) => {
+const nodeDragSource = DragSource<NodeViewProps>('node', {
+  beginDrag: (props, monitor, component) => ({
+    nodeId: props.nodeId
+  }),
+  endDrag: (props, monitor, component) => {
+    const dropResult = monitor.getDropResult();
+
+    if (!dropResult || !dropResult['nodeId']) {
+      return;
+    }
+
+    try {
+      const targetNode = nodeStore.getNode(dropResult['nodeId']);
+      const sourceNode = nodeStore.getNode(props.nodeId);
+      sourceNode.setParent(targetNode);
+    } catch (e) {
+      console.error('Error finalizing drag operation', e);
+      return;
+    }
+  }
+}, (connect, monitor) => {
+  return {
+    connectDragSource: connect.dragSource()
+  };
+});
+
+const nodeDropTarget = DropTarget<NodeViewProps>('node', {
+  drop: (props, monitor, component) => {
+    if (monitor.isOver({ shallow: true })) {
+      return {
+        nodeId: props.nodeId
+      };
+    }
+  }
+}, (connect, monitor) => {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    hasDraggingOver: monitor.isOver({ shallow: true })
+  };
+});
+
+export default nodeDragSource(nodeDropTarget(CSSModule(observer<NodeViewProps>(props => {
 
   let node;
 
   try {
-    node = nodeStore.getNode(nodeId);
+    node = nodeStore.getNode(props.nodeId);
   } catch (e) {
     return <div />;
   }
 
   const NodeTypeComponent = require('../node-types/' + NodeType[node.type].toLowerCase()).default;
 
-  const isOutlined = node.id === uiState.hoveredNode;
+  const isOutlined = node.id === uiState.hoveredNode || props['hasDraggingOver'];
   const nodeStyles = ['node', isOutlined ? 'outlined' : ''].join(' ');
 
-  return <div styleName={nodeStyles}>
+  return props['connectDragSource'](props['connectDropTarget'](<div styleName={nodeStyles}>
     <Handle node={node} />
     <NodeTypeComponent node={node} />
-  </div>;
-}), styles, { allowMultiple: true });
+  </div>));
+}), styles, { allowMultiple: true })));
