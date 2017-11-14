a0946feeba3025be583543c1135d85dde54314b5
Luke Turner <github@luketurner.org>
2017-11-13 17:15:36 -0800

    Fix production build

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,20 +1,20 @@
-var webpack = require('webpack')
-var config = require('./webpack.base.conf')
+var webpack = require('webpack');
+var config = require('./webpack.base.conf');
 
 // eval-source-map is faster for development
-config.devtool = 'eval-source-map'
+config.devtool = 'eval-source-map';
 
 // add sourcemap
-config.module.rules.push()
+config.module.rules.push();
 
 // necessary for the html plugin to work properly
 // when serving the html from in-memory
-config.output.publicPath = '/'
+config.output.publicPath = '/';
 
 config.plugins = (config.plugins || []).concat([
   // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
   new webpack.HotModuleReplacementPlugin(),
   new webpack.NoEmitOnErrorsPlugin()
-])
+]);
 
-module.exports = config
+module.exports = config;
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,28 +1,24 @@
-var webpack = require('webpack')
-var config = require('./webpack.base.conf')
+var webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+var config = require('./webpack.base.conf');
 
 // naming output files with hashes for better caching.
-// dist/index.html will be auto-generated with correct URLs.
+// dist/index.html will be auto-generated with correct URL.
 // config.output.filename = '[name].[chunkhash].js'
-config.output.chunkFilename = '[id].[chunkhash].js'
+config.output.chunkFilename = '[id].[chunkhash].js';
 
-// whether to generate source map for production files.
-// disabling this can speed up the build.
-var SOURCE_MAP = true
-
-config.devtool = SOURCE_MAP ? 'source-map' : false
+config.devtool = 'source-map';
 
 config.plugins = (config.plugins || []).concat([
   new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: '"production"'
-    }
+    'process.env.NODE_ENV': 'production'
   }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false
+  new UglifyJSPlugin({
+    uglifyOptions: {
+      ecma: 6
     }
   })
-])
+]);
 
-module.exports = config
+module.exports = config;
