6024b676b9ed8605daaae1cee1e056a04d8dab0b
Scripsi Deployment <github@luketurner.org>
2017-11-16 20:34:14 -0800

    Work on Circle deploy script

--- build/deploy.sh
+++ build/deploy.sh
@@ -10,6 +10,5 @@ git commit -m "Automatic Deployment"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "$1" master:gh-pages > /dev/null 2>&1
+# will be lost, since we are overwriting it.)
+git push --force --quiet "$1" master:gh-pages
