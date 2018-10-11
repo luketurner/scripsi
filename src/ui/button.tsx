18e54be5e7430928c13f09f7cff0a48273db2559
Scripsi Deployment <github@luketurner.org>
2018-10-11 12:45:06 -0700

    Improve Button API

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -3,14 +3,12 @@ import * as React from 'react';
 import { Icon, IconType } from './icon';
 
 export interface ButtonProps {
-  icon?: IconType;
-  text?: string;
   onClick: (e: any) => void;
+  children?: React.ReactNode;
 }
 
-export const Button = ({ icon, text, onClick }: ButtonProps) => (
+export const Button = ({ children, onClick }: ButtonProps) => (
   <div className='cursor-pointer hover:bg-grey-light h-full p-2' onClick={onClick}>
-    { icon ? <Icon type={icon} /> : '' }
-    { text || '' }
+    { children }
   </div>
 );
