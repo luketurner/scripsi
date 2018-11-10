c03cbfe300980eb9dd53ac0ab6950801bbd901cc
Scripsi Deployment <github@luketurner.org>
2018-11-09 19:39:29 -0800

    Add observable selection utility (currently unused)

--- src/util.ts
+++ src/util.ts
@@ -1,1 +1,11 @@
+import { observable } from "mobx";
+
 export const isDevelopment = process.env.NODE_ENV === 'development';
+
+// Convert "evented" selection API into "observable" one so we can easily use it in component logic.
+// TODO -- this seems a bit weird. Boxing the collection directly doesn't seem to work, unless I'm missing something.
+const selection = observable.box({ current: window.getSelection() }, { deep: false });
+document.addEventListener('selectionchange', () => selection.set({ current: window.getSelection() }));
+
+// Expose read-only access
+export const getSelection = () => selection.get().current;
