636579f76a92a2bb91fe427ef99e54b4424cbb33
Scripsi Deployment <github@luketurner.org>
2018-11-11 07:19:41 -0800

    Improve appearance of settings view

--- src/ui/settings-view/index.tsx
+++ src/ui/settings-view/index.tsx
@@ -15,23 +15,23 @@ export const SettingsView = observer(() => {
     const isSecondary = settings.backupBackendIds.includes(id);
     return (
       <tr>
-        <td className='p-2'>
+        <td>
           {backendSettings.name}
         </td>
         <td>
-         <div className='p-2 text-grey'>{ isReady ? 'Ready' : isPreAuth ? 'Working...' : 'Not Ready' }</div>
+         <div className='text-grey'>{ isReady ? 'Ready' : isPreAuth ? 'Working...' : 'Not Ready' }</div>
         </td>
         <td>
           { !isReady
             ? <Button onClick={() => storageDriver.authenticateBackend(id)}>ENABLE</Button>
             : isPrimary
-            ? <div className='p-2 text-grey'>ON</div>
+            ? <div className='text-grey'>ON</div>
             : <Button onClick={() => settings.primaryBackendId = id}>OFF</Button>
           }
         </td>
         <td>
           { !isReady || isPrimary
-            ? <div className='p-2 text-grey'>N/A</div>
+            ? <div className='text-grey'>N/A</div>
             : isSecondary
             ? <Button onClick={() => settings.backupBackendIds['remove'](id)}>ON</Button>
             : <Button onClick={() => settings.backupBackendIds.push(id)}>OFF</Button>
@@ -44,11 +44,13 @@ export const SettingsView = observer(() => {
   return (
     <div>
       <h1>Settings</h1>
+      <p>
+        This page allows you to configure Scripsi's behavior. Settings are saved in your browser's local storage.
+      </p>
       <h2>Backends</h2>
       <p>
-        Scripsi is designed to give you maximum control over your data by allowing you to configure where it is stored.
-        By default, data is only stored in your Web browser on your computer. However, Scripsi does support storing
-        data in the cloud using Dropbox.
+        Scripsi allows you to control where your data is stored. By default, data is only stored in your Web browser on your computer.
+        However, Scripsi can also use Dropbox as a storage backend, allowing you to access your data across different browsers.
       </p>
       <p>
         Use the table below to configure where Scripsi reads and writes data. You must have exactly one read/write
@@ -58,6 +60,7 @@ export const SettingsView = observer(() => {
         <thead>
           <tr>
             <th className='w-32'>Backend</th>
+            <th className='w-16'>Status</th>
             <th className='w-16'>Read/Write</th>
             <th className='w-16'>Backup</th>
           </tr>
@@ -66,8 +69,6 @@ export const SettingsView = observer(() => {
           {Array.from(settings.backends.entries()).map(([id, s]) => <BackendRow id={id} settings={s} key={id} />)}
         </tbody>
       </table>
-      <h2>Dropbox</h2>
-      <DropboxSettingsView />
     </div>
   );
 });
