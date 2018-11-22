2a276867a14a329720f1b533e886c00456a9ee70
Scripsi Deployment <github@luketurner.org>
2018-11-22 12:15:13 -0800

    Add hydrate method to settings object

--- src/settings/settings-object.ts
+++ src/settings/settings-object.ts
@@ -1,6 +1,10 @@
 export abstract class SettingsObject {
   constructor(params?: Partial<SettingsObject>) {
     if (!params) return;
+    this.hydrate(params);
+  }
+
+  public hydrate(params: Partial<SettingsObject>) {
     Object.assign(this, params);
   }
 }
