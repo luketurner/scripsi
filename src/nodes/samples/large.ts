06c90ca2f3ad8016dcd883e19b63879bf76d1d37
Scripsi Deployment <github@luketurner.org>
2018-11-25 16:42:37 -0800

    Make large document be actually large

--- src/nodes/samples/large.ts
+++ src/nodes/samples/large.ts
@@ -1,14 +1,18 @@
+import { range } from 'lodash/fp';
 import { NodeType } from '..';
 import { makeIndex } from '.';
+
+
+const makeChildTree = (count, collapsed = false) => range(0, count).map(i => ({
+  content: `child number ${i}`,
+  children: makeChildTree(--count),
+  collapsed
+}));
 
 export const largeDocument = () => {
   return makeIndex({
     content: 'Large Document',
     type: NodeType.Heading,
-    children: [
-      {
-        content: 'Text Nodes'
-      }
-    ]
+    children: makeChildTree(10, true),
   });
 };
